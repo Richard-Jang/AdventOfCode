11db2e2fb29373e3b940072ac0b7b302f36fdb86
o7 <111016108+Richard-Jang@users.noreply.github.com>
2023-12-06 20:47:48 +0000

    day3 uncomplete

--- 2023/day3/solution1.py
+++ 2023/day3/solution1.py
@@ -12,25 +12,36 @@ def main():
                     symbolMap[index].append(charIndex)
     for lineNum in range(len(input)):
         if lineNum == 0:
+            #sum += int(checkAdjacent(symbolMap[lineNum], input[lineNum]) or 0)
+            #print(symbolMap[lineNum], input[lineNum], 'head')
+            #print(symbolMap[lineNum + 1], input[lineNum + 1], 'head')
+            #sum += int(checkAdjacent(symbolMap[lineNum + 1], input[lineNum + 1]) or 0)
             sum += int(checkAdjacent(symbolMap[lineNum], input[lineNum]) or 0)
-            sum += int(checkAdjacent(symbolMap[lineNum + 1], input[lineNum + 1]) or 0)
+            sum += int(checkAdjacent(symbolMap[lineNum], input[lineNum + 1]) or 0)
         elif lineNum == (len(input) - 1):
+            sum += int(checkAdjacent(symbolMap[lineNum], input[lineNum - 1]) or 0)
             sum += int(checkAdjacent(symbolMap[lineNum], input[lineNum]) or 0)
-            sum += int(checkAdjacent(symbolMap[lineNum - 1], input[lineNum - 1]) or 0)
+            #print(symbolMap[lineNum], input[lineNum], 'end')
+            #sum += int(checkAdjacent(symbolMap[lineNum], input[lineNum]) or 0)
+            #sum += int(checkAdjacent(symbolMap[lineNum - 1], input[lineNum - 1]) or 0)
         else:
+            sum += int(checkAdjacent(symbolMap[lineNum], input[lineNum - 1]) or 0)
             sum += int(checkAdjacent(symbolMap[lineNum], input[lineNum]) or 0)
-            sum += int(checkAdjacent(symbolMap[lineNum - 1], input[lineNum - 1]) or 0)
-            sum += int(checkAdjacent(symbolMap[lineNum + 1], input[lineNum + 1]) or 0)
+            sum += int(checkAdjacent(symbolMap[lineNum], input[lineNum + 1]) or 0)
+            #print(symbolMap[lineNum], input[lineNum], 'middle')
+            #sum += int(checkAdjacent(symbolMap[lineNum + 1], input[lineNum + 1]) or 0)
+            #sum += int(checkAdjacent(symbolMap[lineNum], input[lineNum]) or 0)
+            #sum += int(checkAdjacent(symbolMap[lineNum - 1], input[lineNum - 1]) or 0)
+
     print(sum)
 
-def checkAdjacent(symbolArray, string):                                                                  # check for numbers in adjacent tiles on one line
+def checkAdjacent(symbolArray, string):                                                                  # left and right; check for numbers in adjacent tiles on one line, symbolArray (mapping index of location), string (line to search)
     for num in symbolArray:
         if (num > 0) and (string[num - 1].isnumeric()):                                                  # if -1 has number
             return checkFront(num, string)
-        if string[num].isnumeric():                                                                      # if +0 has number
+        elif string[num].isnumeric():                                                                      # if +0 has number
             return checkFront(num, string) + string[num] + checkBack(num, string)
-        if (num < (len(string) - 1)) and string[num + 1].isnumeric():                                    # if +1 has number
-            print(checkBack(num, string))
+        elif (num < (len(string) - 1)) and string[num + 1].isnumeric():                                    # if +1 has number
             return checkBack(num, string)
 
 def checkFront(index, string):
@@ -38,7 +49,7 @@ def checkFront(index, string):
     finalString = ''
     while (currentNum >= 0) and string[currentNum].isnumeric():
         finalString = string[currentNum] + finalString
-        currentNum += 1
+        currentNum -= 1
     return finalString
 
 def checkBack(index, string):
